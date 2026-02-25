8bd037b0a1c33138b717333561ce20e42867dcc6
Edward Flemming <flemming@mit.edu>
2026-02-24 19:51:52 -0500

    update lme script

--- 3 mixed effects models/lme models26.R
+++ 3 mixed effects models/lme models26.R
@@ -114,17 +114,23 @@ s = sd(CVC$F2V, na.rm=TRUE)
 coefs = fixef(lmec5scale)
 
 # b0
-coefs[1]-coefs[4]*m/s
+b0 = coefs[1]-coefs[4]*m/s
 # b_d
-coefs[2]-coefs[5]*m/s
+b_d = coefs[2]-coefs[5]*m/s
 # b_g
-coefs[3]-coefs[6]*m/s
+b_g = coefs[3]-coefs[6]*m/s
 # b_F2V
-coefs[4]/s
+b_F2V = coefs[4]/s
 # b_d:F2V
-coefs[5]/s
+b_dF2V = coefs[5]/s
 # b_g:F2V
-coefs[6]/s
+b_gF2V = coefs[6]/s
+
+c(b0, b_d, b_g, b_F2V, b_dF2V, b_gF2V)
+
+# compare coefficients of model with unscaled F2V 
+fixef(lmec5)
+
 
 ## 3. recompute gradient and Hessian with Richardson extrapolation
 devfun <- update(lmec5, devFunOnly=TRUE)
@@ -154,104 +160,4 @@ library(blme)
 blmem = blmer(intensity~affricate*context+(affricate*context|subject)+(context|word), data=aff)
 summary(blmem)
 
-# scaling and centering a variable for each subject
 
-# using dplyr
-CVC = mutate(CVC, .by=speaker, F2Vnorm = scale(F2V))
-
-# using tapply
-# centering by subject
-CVC$F2Vnorm = CVC$F2V-tapply(CVC$F2V,CVC$speaker,mean,na.rm=T)[CVC$speaker]
-
-#normalizing by subject
-CVC$F2Vnorm = (CVC$F2V-tapply(CVC$F2V,CVC$speaker,mean,na.rm=T)[CVC$speaker])/tapply(CVC$F2V,CVC$speaker,sd,na.rm=T)[CVC$speaker]
-
-# Fit linear mixed models to the CVC data where C1 is [b] or [d]
-CVCbd = filter(CVC, C1 =="b"|C1=="d")
-CVCbd$C1 = factor(CVCbd$C1)
-
-CVCbd$F2Vscale = scale(CVCbd$F2V)
-
-lmbd = lm(F2C1~C1*F2V, data=CVCbd)
-summary(lmbd)
-
-lmebd_int = lmer(F2C1~C1*F2V+(1|speaker), data = CVCbd)
-summary(lmebd_int)
-
-lmebd_intC1 = lmer(F2C1~C1*F2V+(1+C1|speaker), data = CVCbd)
-summary(lmebd_intC1)
-
-lmebd_max = lmer(F2C1~C1*F2V+(C1*F2V|speaker), data = CVCbd)
-
-lmebd2 = lmer(F2C1~C1*F2V+(C1+F2V|speaker), data = CVCbd)
-summary(lmebd2)
-
-lmebd2_all = allFit(lmebd2)
-summary(lmebd2_all)
-
-lmebd2scale = lmer(F2C1~C1*F2Vscale+(C1+F2Vscale|speaker), data = CVCbd)
-summary(lmebd2scale)
-
-# try fitting models with normalized F2V
-lmebd2norm = lmer(F2C1~C1*F2Vnorm+(C1+F2Vnorm|speaker), data = CVCbd)
-summary(lmebd2norm)
-
-# singular
-lmec_norm_max = lmer(F2C1~C1*F2Vnorm+(C1*F2Vnorm|speaker), data = CVC)
-
-# convergence warning
-lmec_norm2 = lmer(F2C1~C1*F2Vnorm+(C1+F2Vnorm|speaker), data = CVC)
-
-lmec_norm3 = lmer(F2C1~C1*F2Vnorm+(C1|speaker), data = CVC)
-
-
-# try all optimizers
-lmec_norm2_all = allFit(lmec_norm2)
-summary(lmec_norm2_all)
-
-# model F2C2
-CVC$C2=factor(CVC$C2)
-contrasts(CVC$C2) = contr.sum(levels(CVC$C2))
-
-# use blmer to try to avoid singular fit/convergence problems
-# default priors
-blmec = blmer(F2C1~C1*F2V+(C1*F2V|speaker), data = CVCbd)
-
-blmecscale = blmer(F2C1~C1*F2Vscale+(C1*F2Vscale|speaker), data = CVCbd)
-
-blmecnorm = blmer(F2C1~C1*F2Vnorm+(C1*F2Vnorm|speaker), data = CVCbd)
-summary(blmecnorm)
-
-blmec = blmer(F2C1~C1*F2Vnorm+(C1*F2Vnorm|speaker), data = CVCbd)
-summary(blmec)
-
-## rosa's roses data
-schwa = read.csv("schwa_minimalpairs.csv")
-
-schwa$word = tolower(schwa$word)
-schwa$category = tolower(schwa$category)
-
-schwa$speaker = factor(schwa$speaker)
-
-schwa_lme = lmer(F1~category+(1+category|speaker), schwa)
-
-schwa_lme2 = lmer(F1~category+(1+category|speaker)+(1|word), schwa)
-summary(schwa_lme2)
-
-schwa_lme3 = lmer(F1~category+(1+category|speaker), schwa)
-
-anova(schwa_lme2, schwa_lme3)
-
-schwa_lme4 = lmer(F1~category+(1|speaker)+(1|word), schwa)
-
-anova(schwa_lme2, schwa_lme4)
-
-
-schwa_lme2 = lmer(F1~category+(1+category|speaker)+(1+category|pair), schwa)
-summary(schwa_lme2)
-
-schwa_lme3 = lmer(F1~category+(1+category|speaker)+(1|pair), schwa)
-summary(schwa_lme3)
-
-schwaF2_lme2 = lmer(F2~category+(1+category|speaker)+(1|pair), schwa)
-summary(schwaF2_lme2)
